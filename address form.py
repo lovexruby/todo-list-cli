5cfc49b19dd88b64f30f6425e5ce0e9f05a788be
lovexruby <ruben.menc@hotmail.com>
2025-03-06 12:18:16 +0100

    German bureucracy outdated form workin, not clean, no comments

--- address form.py
+++ address form.py
@@ -2,17 +2,16 @@ import tkinter as tk
 from tkinter import ttk
 import json
 import os
-import subprocess
 
 
-FORM_FILE = "form_file"
+FORM_FILE = "form_file" #Name the file
 
 
 def save_form(form):
-    with open(FORM_FILE, "w") as file:
-        json.dump(form, file, indent = 4)
+    with open(FORM_FILE, "w") as file_var:
+        json.dump(form, file_var, indent = 4)
 
-def clearButton_clicked():
+def clear_button_clicked():
     fnEntry.delete(0, tk.END)
     lnEntry.delete(0, tk.END)
     A1Entry.delete(0, tk.END)
@@ -24,7 +23,7 @@ def clearButton_clicked():
 
 
 
-def submitButton_clicked():
+def submit_button_clicked():
     first_name = fnEntry.get()
     last_name = lnEntry.get()
     address1 = A1Entry.get()
@@ -47,69 +46,71 @@ def submitButton_clicked():
 
     save_form(form)
     json_file_path = os.path.abspath(FORM_FILE)
-    #os.system(f"pycharm {json_file_path}")
+    os.system(f"pycharm {json_file_path}")
 
-    subprocess.run(["C:\Program Files\JetBrains\PyCharm Community Edition 2024.3.4\bin\pycharm64.exe", json_file_path])
+    #subprocess.run(["C:\Program Files\JetBrains\PyCharm Community Edition 2024.3.4\bin\pycharm64.exe", json_file_path])
 
 window = tk.Tk()
 
-window.rowconfigure([0,1,2,3,4,5,6,7,8], minsize=10)
-window.columnconfigure([0,1,2], minsize=0)
+window.title("German Outdated Form")
+window.minsize(300,200)
+
+window.rowconfigure([0,1,2,3,4,5,6,7,8], weight = 1, minsize=10)
+window.columnconfigure([0,1,2], weight = 1, minsize=0)
 
 
-labelFN = tk.Label(text = "First Name:")
-fnEntry = tk.Entry()
-labelLN = tk.Label(text = "Last Name:")
-lnEntry = tk.Entry()
-labelA1 = tk.Label(text = "Address 1:")
-A1Entry = tk.Entry()
-labelA2 = tk.Label(text = "Address 2:")
-A2Entry = tk.Entry()
-labelCity = tk.Label(text = "City:")
-cityEntry = tk.Entry()
-labelSP = tk.Label(text = "State/Province:")
-spEntry = tk.Entry()
-labelPC = tk.Label(text = "Postal Code:")
-pcEntry = tk.Entry()
-labelCountry = tk.Label(text = "Country:")
-countryEntry = tk.Entry()
-
+labelFN = ttk.Label(text = "First Name:")
+fnEntry = ttk.Entry()
+labelLN = ttk.Label(text = "Last Name:")
+lnEntry = ttk.Entry()
+labelA1 = ttk.Label(text = "Address 1:")
+A1Entry = ttk.Entry()
+labelA2 = ttk.Label(text = "Address 2:")
+A2Entry = ttk.Entry()
+labelCity = ttk.Label(text = "City:")
+cityEntry = ttk.Entry()
+labelSP = ttk.Label(text = "State/Province:")
+spEntry = ttk.Entry()
+labelPC = ttk.Label(text = "Postal Code:")
+pcEntry = ttk.Entry()
+labelCountry = ttk.Label(text = "Country:")
+countryEntry = ttk.Entry()
 
 
 labelFN.grid(row=0, column=0, sticky = "e")
-fnEntry.grid(row=0, column=1, columnspan=2)
+fnEntry.grid(row=0, column=1, columnspan=2, sticky = "ew")
 labelLN.grid(row=1, column=0, sticky = "e")
-lnEntry.grid(row=1, column=1, columnspan=2)
+lnEntry.grid(row=1, column=1, columnspan=2, sticky = "ew")
 labelA1.grid(row=2, column=0, sticky = "e")
-A1Entry.grid(row=2, column=1, columnspan=2)
+A1Entry.grid(row=2, column=1, columnspan=2, sticky = "ew")
 labelA2.grid(row=3, column=0, sticky = "e")
-A2Entry.grid(row=3, column=1, columnspan=2)
+A2Entry.grid(row=3, column=1, columnspan=2, sticky = "we")
 labelCity.grid(row=4, column=0, sticky = "e")
-cityEntry.grid(row=4, column=1, columnspan=2)
+cityEntry.grid(row=4, column=1, columnspan=2, sticky = "ew")
 labelSP.grid(row=5, column=0, sticky = "e")
-spEntry.grid(row=5, column=1, columnspan=2)
+spEntry.grid(row=5, column=1, columnspan=2, sticky = "ew")
 labelPC.grid(row=6, column=0, sticky = "e")
-pcEntry.grid(row=6, column=1, columnspan=2)
+pcEntry.grid(row=6, column=1, columnspan=2, sticky = "ew")
 labelCountry.grid(row=7, column=0, sticky = "e")
-countryEntry.grid(row=7, column=1, columnspan=2)
+countryEntry.grid(row=7, column=1, columnspan=2, sticky = "ew")
 
 
 
 
 
-clearButton = tk.Button(window,
+clearButton = ttk.Button(window,
                         text = "Clear",
-                        command = clearButton_clicked)
+                        command = clear_button_clicked)
 submitButton = ttk.Button(window,
                           text = "Submit",
-                          command = submitButton_clicked)
+                          command = submit_button_clicked)
 
 
 
 
 
-clearButton.grid(row=8, column=1)
-submitButton.grid(row = 8, column = 2)
+clearButton.grid(row=8, column=1, sticky = "ew")
+submitButton.grid(row = 8, column = 2, sticky = "ew")
 
 
 
